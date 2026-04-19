33b3c5489118576e8c92aad2637dca4ad47da0cc
Eugeniusz Dzikowicz <eugeniusz.dzikowicz@gmail.com>
2026-04-19 10:04:13 +0200

    Improved implementation of Injector service

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -14,6 +13,10 @@ import mate.academy.service.impl.ProductServiceImpl;
 
 public class Injector {
     private static final Injector injector = new Injector();
+    private static Map<Class<?>, Class<?>> interfaceImplMap = Map.of(
+            ProductService.class, ProductServiceImpl.class,
+            FileReaderService.class, FileReaderServiceImpl.class,
+            ProductParser.class, ProductParserImpl.class);
     private Map<Class<?>, Object> instances = new HashMap<>();
 
     public static Injector getInjector() {
@@ -23,23 +26,29 @@ public class Injector {
     public Object getInstance(Class<?> interfaceClazz) {
         Class<?> clazz = findImpl(interfaceClazz);
         Object clazzImplInstance = null;
-        Field[] fields = interfaceClazz.getDeclaredFields();
-        for (Field field : fields) {
-            if (field.isAnnotationPresent(Inject.class)) {
-                Object instance = injector.getInstance(field.getType());
-                clazzImplInstance = createNewInstance(clazz);
-                try {
-                    field.setAccessible(true);
-                    field.set(clazzImplInstance, instance);
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException("Can't initialized field. Class: " + clazz.getName()
-                            + ". Field: " + field.getName());
 
+        if (clazz.isAnnotationPresent(Component.class)) {
+            Field[] fields = clazz.getDeclaredFields();
+            for (Field field : fields) {
+                if (field.isAnnotationPresent(Inject.class)) {
+                    Object instance = injector.getInstance(field.getType());
+                    clazzImplInstance = createNewInstance(clazz);
+                    try {
+                        field.setAccessible(true);
+                        field.set(clazzImplInstance, instance);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException("Can't initialized field. Class: "
+                                + clazz.getName()
+                                + ". Field: " + field.getName(), e);
+                    }
                 }
             }
-        }
-        if (clazzImplInstance == null) {
-            clazzImplInstance = createNewInstance(clazz);
+            if (clazzImplInstance == null) {
+                clazzImplInstance = createNewInstance(clazz);
+            }
+        } else {
+            throw new RuntimeException("Class: " + clazz.getName()
+                    + " is not annotated with @Component");
         }
         return clazzImplInstance;
     }
@@ -55,25 +64,16 @@ public class Injector {
             instances.put(clazz, instance);
             return instance;
 
-        } catch (NoSuchMethodException | IllegalAccessException
-                 | InstantiationException | InvocationTargetException e) {
-            throw new RuntimeException("Cannot create instance of " + clazz.getName());
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("Cannot create instance of " + clazz.getName(), e);
         }
     }
 
     private Class<?> findImpl(Class<?> interfaceClazz) {
-        Map<Class<?>, Class<?>> interfaceImplMap = new HashMap<>();
-        interfaceImplMap.put(ProductService.class, ProductServiceImpl.class);
-        interfaceImplMap.put(FileReaderService.class, FileReaderServiceImpl.class);
-        interfaceImplMap.put(ProductParser.class, ProductParserImpl.class);
-
-        if (!interfaceClazz.isAnnotationPresent(Component.class)) {
-            throw new RuntimeException(
-                    "Injection failed, missing @Component annotation on the class "
-                            + interfaceClazz.getName());
-        }
-
         if (interfaceClazz.isInterface()) {
+            if (!interfaceImplMap.containsKey(interfaceClazz)) {
+                throw new RuntimeException("Cannot find class: " + interfaceClazz.getName());
+            }
             return interfaceImplMap.get(interfaceClazz);
         }
 
